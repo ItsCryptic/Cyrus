1b1ecf8050baf333b677acf291b87bb63078bd34
Kevin D <kevin.diangelis@gmail.com>
2022-09-20 18:32:05 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const fs = require("fs");
+const path = require('path')
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers]});
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file =>  file.endsWith('.js'));
+
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    }else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
+client.on("messageCreate", (message) => {
+        if (message.author.bot) return false;
+    
+        if (message.content.includes("@here") || message.content.includes("@everyone") || message.type == "REPLY") return false;
+    
+        if (message.mentions.has(client.user.id)) {
+            message.channel.send("Howdy!");
+        }
+    });
+
+client.login(token);
